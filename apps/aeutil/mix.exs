69148b50f13557c481237593d33f0900f919a43a
Oleg Ivanov <oleg.ivanov@quanterall.com>
2018-06-08 16:06:46 +0300

    [GH-#307] added first version of testing framework

--- apps/aeutil/mix.exs
+++ apps/aeutil/mix.exs
@@ -13,7 +13,8 @@ defmodule Aeutil.Mixfile do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger]
+      extra_applications: [:logger],
+      applications: [:porcelain]
     ]
   end
 end
